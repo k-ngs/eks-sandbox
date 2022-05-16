5e2741fbdbd77173cc8a32c53f9bdb33f6d0fd46
Kohei Nagase <kohei.nagase@3-shake.com>
2022-05-16 11:52:07 +0900

    version up aws provider and k8s provider

--- env/sandbox/main.tf
+++ env/sandbox/main.tf
@@ -3,9 +3,10 @@ data "aws_availability_zones" "available" {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "eks-sandbox-vpc"
-  cidr   = "10.9.0.0/16"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+  name    = "eks-sandbox-vpc"
+  cidr    = "10.9.0.0/16"
 
   azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.9.1.0/24", "10.9.2.0/24"]
@@ -20,10 +21,10 @@ module "eks_cluster" {
   source             = "../../modules/eks"
   vpc_id             = module.vpc.vpc_id
   cluster_name       = "eks-sandbox-cluster"
-  cluster_version    = "1.21"
+  cluster_version    = "1.20"
   subnet_ids         = module.vpc.private_subnets
   private_subnet_ids = module.vpc.private_subnets
-  map_users = var.map_users
+  map_users          = var.map_users
 
   tags = local.default_tags
 }
